a54ae99a71198034e4fcc7aa3d3607b8509aed5b
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-06-19 12:13:44 -0300

    Navbar Finished
    Finished all the navbar features, ready to work on the next sections.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,43 +1,104 @@
+'use client'
 import { LuUserRound } from "react-icons/lu";
 import { IoBagHandleOutline } from "react-icons/io5";
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Navbar() {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen)
+    };
+
     return (
         <>
         <header className="header">
-            <nav>
+            <nav className="relative z-[999]">
+                {/* Desktop version from the navbar */}
                 <div className="desktop-navbar">
                     <div className="flex justify-evenly py-5 items-center">
                         <a href="#Home" className="font-black text-3xl uppercase">Logo</a>
                     </div>
                     <ul className="navbar-links">
-                        <li className="links">Contact</li>
-                        <li className="links">Trending</li>
-                        <li className="links">Male</li>
-                        <li className="links">Women</li>
-                        <li className="links">Brands</li>
+                        <li className="links">
+                            <Link href="/Contact">Contact</Link>
+                        </li>
+                        <li className="links">
+                            <Link href="/Men">Men</Link>
+                        </li>
+                        <li className="links">
+                            <Link href="/Women">Women</Link>
+                        </li>
+                        <li className="links">
+                            <Link href="/Trending">Trending</Link>
+                        </li>
+                        <li className="links">
+                            <Link href="/Brands">Brands</Link>
+                        </li>
                         <li className="inline-flex flex-row px-5 gap-5">
-                            <a href="#User" className="navbar-icons">
+                            <Link href="/Login" className="links">
                                 <LuUserRound />
-                            </a>
-                            <a href="#Cart" className="navbar-icons">
+                            </Link>
+                            <Link href="/Shopping" className="links">
                                 <IoBagHandleOutline />
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
+                {/* Mobile version from the navbar */}
                 <div className="mobile-navbar">
-                    <div className="flex items-center py-5">
-                        <a href="#Home" className="font-black text-3xl uppercase">Logo</a>
+                    <div className="flex items-center">
+                        <Link href="/Home" className="font-black text-3xl uppercase">Logo</Link>
                     </div>
-                    <div className="flex rounded-full m-5 p-5 active:bg-neutral-800 active:text-neutral-100 transition-all ease-in duration-200 items-end py-5">
-                        <a href="" className=" text-xl">
-                            <GiHamburgerMenu />
-                        </a>
-                    </div>
+                    <button 
+                     className="p-5 rounded-full active:text-neutral-100 transition-colors ease-in duration-200"
+                     onClick={toggleMobileMenu}
+                     aria-label="Open Menu"
+                     >
+                        <GiHamburgerMenu />
+                    </button>
                 </div>
+                {/* Overlay to close the menu when click outside */}
+                {isMobileMenuOpen && (
+                    <div 
+                     className="fixed inset-0 bg-neutral-800/50 z-[1000] cursor pointer animate-fadeIn"
+                     onClick={toggleMobileMenu}
+                     ></div>
+                )}
+                {/* Sidebar from mobile menu */}
+                <nav 
+                className={`fixed top-0 w-64 h-fulll bg-gray-100 shadow-lg z-[1002] pt-16 transition-all duration-200 ease-in-out
+                    ${isMobileMenuOpen ? 'right-0' : '-right-64'}
+                    `}
+                >
+                    <ul className="list-none p-0 m-0">
+                        <li className="mobile-navbar-links">
+                            <Link href="/Contact" onClick={toggleMobileMenu} className="links block">Contact</Link>
+                        </li>
+                        <li className="mobile-navbar-links">
+                            <Link href="/Men" onClick={toggleMobileMenu} className="links block">Men</Link>
+                        </li>
+                        <li className="mobile-navbar-links">
+                            <Link href="/Women" onClick={toggleMobileMenu} className="links block">Women</Link>
+                        </li>
+                        <li className="mobile-navbar-links">
+                            <Link href="/Trending" onClick={toggleMobileMenu} className="links block">Trending</Link>
+                        </li>
+                        <li className="mobile-navbar-links">
+                            <Link href="/Brands" onClick={toggleMobileMenu} className="links block">Brands</Link>
+                        </li>
+                        <li className="py-4 px-5 border-t border-neutral-200 mt-4 flex justify-around">
+                            <Link href="/Login" onClick={toggleMobileMenu} className="links">
+                                <LuUserRound />
+                            </Link>
+                            <Link href="/Shopping" onClick={toggleMobileMenu} className="links">
+                                <IoBagHandleOutline />
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
             </nav>
         </header>
         </>
--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -1,6 +1,17 @@
+'use client'
 import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 export default function Notification() {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleCloseNotification = () => {
+        setIsVisible(false);
+    }
+    if (!isVisible) {
+        return null;
+    }
+
     return (
         <>
         <div className="notification">
@@ -8,9 +19,17 @@ export default function Notification() {
                 <h1>We have a <strong>60% Discount</strong> and <strong>Free Delivery</strong> to new users in anything on our store</h1>
             </div>
             <div>
-                <a href="">
-                    <IoMdClose className="hover:animate-spin hover:text-neutral-800 transition-all ease-in-out duration-200 cursor-pointer" />
-                </a>
+                <IoMdClose 
+                 className="
+                  hover:animate-spin 
+                  hover:text-neutral-800 
+                  transition-all 
+                  ease-in-out 
+                  duration-200 
+                  cursor-pointer
+                  "
+                  onClick={handleCloseNotification}
+                  />
             </div>
         </div>
         </>
