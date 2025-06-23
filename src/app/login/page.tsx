f1a14feb73847291953ee8f3e3b7e5658c1958f4
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-06-23 13:30:29 -0300

    Mobile version from the login and register finished
    finished the mobile version from the login and register pages. now work intense in the hero section.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,8 +7,9 @@ import { FaFacebookSquare } from "react-icons/fa";
 export default function Login() {
     return(
         <section className="section flex justify-center items-center bg-neutral-800">
-            <div className="login-container">
-                <div className="w-full m-3 p-3 px-10 flex flex-row justify-between items-center">
+            {/* Desktop Version */}
+            <div className="login-desktop-container">
+                <div className="login-desktop-logo">
                     <Link href="/" className="login-logo">LOGO</Link>
                     <h1 className="font-light text-2xl capitalize">Your account</h1>
                 </div>
@@ -37,6 +38,41 @@ export default function Login() {
                 <div className="flex flex-col items-center gap-6">
                     <button className="bg-neutral-900 rounded-xl text-neutral-200 p-4 px-7 text-lg hover:border hover:border-neutral-950 hover:bg-transparent hover:text-neutral-950 transition-colors ease-in-out duration-200 hover:shadow-xl cursor-pointer"><Link href="/">Login now</Link></button>
                     <div className="">
+                        <p className="cursor-default">Don't have an account? <Link href="/register" className="text-neutral-600 underline cursor-pointer">Register now</Link></p>
+                    </div>
+                </div>
+            </div>
+            {/* Mobile Vesion */}
+            <div className="mobile-login-container">
+                <div className="mobile-logo-container">
+                    <Link href="/" className="mobile-logo">LOGO</Link>
+                    <h1 className="font-light text-lg capitalize">Your Account</h1>
+                </div>
+                <div className="w-[60%] h-0.5 rounded-full bg-neutral-950"></div>
+                 <div className="flex flex-row items-center gap-2 py-4">
+                    <div className="mobile-auth-socials">
+                        <FcGoogle />
+                    </div>
+                    <div className="mobile-auth-socials text-neutral-600">
+                        <SiApple />
+                    </div>
+                    <div className="mobile-auth-socials text-blue-600">
+                        <FaFacebookSquare />
+                    </div>
+                </div>
+                <div className="w-full flex flex-col items-center py-1 mb-3 p-3 gap-6">
+                    <input 
+                     type="email" 
+                     placeholder="Email"
+                     className="mobile-login-input" />
+                    <input 
+                     type="password"
+                     placeholder="Password"
+                     className="mobile-login-input" />
+                </div>
+                <div className="flex flex-col items-center gap-4">
+                    <button className="bg-neutral-900 rounded-xl text-neutral-200 p-3 px-5 text-base hover:border hover:border-neutral-950 hover:bg-transparent hover:text-neutral-950 transition-colors ease-in-out duration-200 hover:shadow-xl cursor-pointer"><Link href="/">Login now</Link></button>
+                    <div className="text-sm">
                         <p className="cursor-default">Don't have an account? <Link href="/register" className="text-neutral-600 underline cursor-pointer">Register now</Link></p>
                     </div>
                 </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,7 +7,8 @@ import { FaFacebookSquare } from "react-icons/fa";
 export default function Login() {
     return(
         <section className="section flex justify-center items-center bg-neutral-800">
-            <div className="login-container">
+            {/* Desktop Version */}
+            <div className="login-desktop-container">
                 <div className="w-full m-3 p-3 px-10 flex flex-row justify-between items-center">
                     <Link href="/" className="login-logo">LOGO</Link>
                     <h1 className="font-light text-2xl capitalize">Your account</h1>
@@ -40,6 +41,42 @@ export default function Login() {
                 </div>
                 <div className="flex flex-col items-center gap-6">
                     <button className="bg-neutral-900 rounded-xl text-neutral-200 p-4 px-7 text-lg hover:border hover:border-neutral-950 hover:bg-transparent hover:text-neutral-950 transition-colors ease-in-out duration-200 hover:shadow-xl cursor-pointer"><Link href="/">Register now</Link></button>
+                </div>
+            </div>
+            {/* Mobile Verison */}
+            <div className="mobile-login-container">
+                <div className="mobile-logo-container">
+                    <Link href="/" className="mobile-logo">LOGO</Link>
+                    <h1 className="font-light text-lg capitalize">Your Account</h1>
+                </div>
+                <div className="w-[60%] h-0.5 rounded-full bg-neutral-950"></div>
+                 <div className="flex flex-row items-center gap-2 py-4">
+                    <div className="mobile-auth-socials">
+                        <FcGoogle />
+                    </div>
+                    <div className="mobile-auth-socials text-neutral-600">
+                        <SiApple />
+                    </div>
+                    <div className="mobile-auth-socials text-blue-600">
+                        <FaFacebookSquare />
+                    </div>
+                </div>
+                <div className="w-full flex flex-col items-center py-1 mb-3 p-3 gap-4">
+                    <input 
+                     type="email" 
+                     placeholder="Email"
+                     className="mobile-login-input" />
+                    <input 
+                     type="password"
+                     placeholder="Password"
+                     className="mobile-login-input" />
+                    <input 
+                     type="password"
+                     placeholder="Confirm Password"
+                     className="mobile-login-input" />
+                </div>
+                <div className="flex flex-col items-center gap-4">
+                    <button className="bg-neutral-900 rounded-xl text-neutral-200 p-3 px-5 text-base hover:border hover:border-neutral-950 hover:bg-transparent hover:text-neutral-950 transition-colors ease-in-out duration-200 hover:shadow-xl cursor-pointer"><Link href="/">Register now</Link></button>
                 </div>
             </div>
         </section>
