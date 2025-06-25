db6eaba63b1b1da56c55734fe02aaf9fd3e8879d
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-06-25 13:02:22 -0300

    added the image from the hero section
    add the image to the start of the hero section creation

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,8 +1,10 @@
+'use client'
+
 export default function Home() {
     return (
         <section className="section">
-            <div className="flex justify-center items-center">
-                <h1>Teste</h1>
+            <div className="w-full inline-flex flex-row justify-evenly items-center">
+                <img src="../public/images/Hero-img.png" alt="" />
             </div>
         </section>
     );
