d06f50d9332857cb5c2e96c507b1c5b2916871db
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-06-25 13:13:27 -0300

    upadate hero 01

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -3,8 +3,10 @@
 export default function Home() {
     return (
         <section className="section">
-            <div className="w-full inline-flex flex-row justify-evenly items-center">
-                <img src="../public/images/Hero-img.png" alt="" />
+            <div className="w-full h-screen inline-flex flex-row justify-evenly items-center">
+                <div className="w-full h-[]">
+                    <img src="/images/Hero-img.png" alt="" />
+                </div>
             </div>
         </section>
     );
