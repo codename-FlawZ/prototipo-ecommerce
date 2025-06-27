5abf0a1bf78a4b80bb388a27d9fe603400a327a9
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-06-27 11:32:37 -0300

    hero section test design
    now provably this will be the final design for the desktop hero section. now we need to work on the mobile version of the hero

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -6,14 +6,13 @@ export default function Home() {
         <section className="relative z-1 w-full min-h-screen section overflow-hidden">
             {/* Desktop Version */}
             <div className="inline-flex flex-col items-center gap-10">
-                <div className="hero-desktop-container">
+                <div className="hero-desktop-container hero-shocase-product-1" style={{ backgroundImage: "url('/images/Hero-img-2.png')"}}>
                 <div className="hero-desktop-showcase-container">
-                    <img src="/images/Hero-img-2.png" alt="the new LNNO Coat" className="hero-img"/>
                 </div>
                 <div className="w-1/2 flex items-center justify-center z-2">
                  <div className="hero-text-container">
                     <p className="hero-title">Now introducing</p>
-                    <h1 className="hero-subtitle">LNNO Coat®</h1>
+                    <h1 className="hero-subtitle">LNNO Boots®</h1>
                     <div className="hero-btn-container">
                         <button className="hero-btn-1"><Link href="/trending">Reach out</Link></button>
                         <button className="hero-btn-2"><Link href="/brands">Know more</Link></button>
@@ -24,7 +23,7 @@ export default function Home() {
             <div className="hero-desktop-container">
                 <div className="w-1/2 flex items-center justify-center">
                  <div className="hero-text-container">
-                    <p className="hero-title">Now introducing</p>
+                    <p className="hero-title">Now Trending</p>
                     <h1 className="hero-subtitle">LNNO Coat®</h1>
                     <div className="hero-btn-container">
                         <button className="hero-btn-1"><Link href="/trending">Reach out</Link></button>
