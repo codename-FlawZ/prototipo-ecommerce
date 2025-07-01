80e03415715d75a61f495a1ac1932fa12d27136c
Josefa Cordeiro Da Silva <133906172+codename-FlawZ@users.noreply.github.com>
2025-07-01 20:53:47 -0300

    little update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -39,7 +39,7 @@ export default function Login() {
                      placeholder="Confirm Password"
                      className="login-input" />
                 </div>
-                <div className="flex items-center pb-5">
+                <div className="flex items-center pb-6">
                     <button className="bg-neutral-900 rounded-xl text-neutral-200 p-4 px-7 text-lg hover:bg-transparent hover:text-neutral-950 transition-colors ease-in-out duration-200 hover:shadow-xl cursor-pointer"><Link href="/">Register now</Link></button>
                 </div>
             </div>
